9ea8db87138a7f7efbfb26b0ef36d639c6aa0e87
Lâm Bá Luân <lambaluan2609@gmail.com>
2025-01-18 15:05:31 +0700

    Layout basic finished: Sidebar and Navbar

--- lms-tutorial/app/(dashboard)/_components/sidebar-item.tsx
+++ lms-tutorial/app/(dashboard)/_components/sidebar-item.tsx
@@ -46,6 +46,11 @@ export const SidebarItem = ({
                 />
                 {label}
             </div>
-        </button>
+            <div 
+                className={cn(
+                    "ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",
+                isActive && "opacity-100"
+            )}/>       
+             </button>
     )
 }
